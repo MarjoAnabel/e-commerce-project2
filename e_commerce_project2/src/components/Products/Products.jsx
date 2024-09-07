f114cad590266a0b3b4167f9489ccf6f13f94ff7
aitziberE <eskizabel.aitziber@gmail.com>
2024-09-07 12:08:15 +0200

    add product card with image

--- e_commerce_project2/src/components/Products/Products.jsx
+++ e_commerce_project2/src/components/Products/Products.jsx
@@ -15,27 +15,54 @@ const Products = () => {
         getProducts()
       }, [])
       
+    // return(
+    //   <main className='products'>
+    //     <ul>
+    //     {products.slice(0, 10).map(product => {
+
+    //       return (
+    //         <li key={product.id}>
+    //            <img
+    //             src={product.images[0]}
+    //             alt={product.name}
+    //           />
+    //           <div>
+    //             <strong>{product.name}</strong> - ${product.price}
+    //           </div>
+    //         </li>
+    //       )
+    //     })}
+    //     </ul>
+    //   </main>
+    // )
     return(
       <main className='products'>
-        <ul>
-        {products.slice(0, 10).map(product => {
-
-          return (
-            <li key={product.id}>
-              <div>
-                <strong>{product.name}</strong> - ${product.price}
+        <div className="card-deck"> 
+          {products.slice(0, 10).map(product => {
+            return (
+              <div className="card">           
+                <img className="card-img-top"
+                  src={product.images[0]}
+                  alt={product.name}
+                />
+                <div className="card-body">
+                <h5 className="card-title"><strong>{product.name}</strong> - ${product.price}</h5>
+                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                {/* si admin mostrar aqui stock disponible en pequeño */}
+                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                </div>
               </div>
-            </li>
-          )
-        })}
-        </ul>
+            )
+          })}
+        </div>
       </main>
     )
+
+    
 
   // return (
   //   <main className='products'>
   //     <ul>
-  //       {console.log("a ver esos productos:" + products) }
   //       {products.slice(0, 10).map(product => {
   //         const isProductInCart = checkProductInCart(product)
 
