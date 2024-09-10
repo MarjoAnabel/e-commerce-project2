44e9ba9218e675bfe92842c1b2a1d625efa70a51
aitziberE <eskizabel.aitziber@gmail.com>
2024-09-10 12:27:59 +0200

    user context

--- src/context/UserContext/UserReducer.jsx
+++ src/context/UserContext/UserReducer.jsx
@@ -1,28 +1,26 @@
 const users = (state, action) => {
-    switch (action.type) {
-      case 'LOGIN':
-        return {
-          ...state,
-          token: action.payload.token,
-        }
-        
-      case 'GET_USER_INFO':
-        return {
-       ...state,
-       user: action.payload,
-     }
-
-
-      case 'LOGOUT':
-        return {
-        user: null,
-        token: null,
+  switch (action.type) {
+    case 'LOGIN':
+      return {
+        ...state,
+        token: action.payload.token,
       }
-   
+      
+    case 'GET_USER_INFO':
+      return {
+      ...state,
+      user: action.payload,
+    }
 
-
-   default:
-     return state
- }
+    case 'LOGOUT':
+      return {
+      user: null,
+      token: null,
+    }
+  
+    default:
+      return state
   }
-   export default users
+}
+
+export default users
--- src/context/UserContext/UserState.jsx
+++ src/context/UserContext/UserState.jsx
@@ -2,13 +2,13 @@ import { createContext, useReducer } from 'react'
 import axios from 'axios'
 import UserReducer from './UserReducer'
 
-const storageToken = localStorage.getItem('token');
+const storageToken = localStorage.getItem('token')
 let parsedToken = null;
 
 try {
-  parsedToken = storageToken ? JSON.parse(storageToken) : null;
+  parsedToken = storageToken ? JSON.parse(storageToken) : null
 } catch (error) {
-  console.error('Error parsing token:', error);
+  console.error('Error parsing token:', error)
   parsedToken = null
 }
 
@@ -24,69 +24,65 @@ export const UserProvider = ({ children }) => {
  
   const login = async (user) => {
     try {
-      const res = await axios.post(`${API_URL}/users/login`, user);
+      const res = await axios.post(`${API_URL}/users/login`, user)
       console.log('Response from API:', res.data);
 
       if (res.data && res.data.token) {
-      localStorage.setItem('token', JSON.stringify(res.data.token));
-      
-      dispatch({
-        type: 'LOGIN',
-        payload: res.data,
-      });
+        localStorage.setItem('token', JSON.stringify(res.data.token))
+        dispatch({
+          type: 'LOGIN',
+          payload: res.data,
+        })
       } else {
-      console.error('Token not found in the response');
+      console.error('Token not found in the response')
       }
     } catch (error) {
       console.error('Error during login:', error);
     }
-    }
+  }
 
-    const getUserInfo = async () => {
-      try {
-        const token = JSON.parse(localStorage.getItem('token'));
-      
-        if (!token) {
-        throw new Error('Token not found');
-        }
-      
-        const res = await axios.get(`${API_URL}/users/userLogin`, {
+  const getUserInfo = async () => {
+    try {
+      const token = JSON.parse(localStorage.getItem('token'))
+    
+      if (!token) {
+        throw new Error('Token not found')
+      }
+    
+      const res = await axios.get(`${API_URL}/users/userLogin`, {
         headers: {
-          authorization: token,
-        },
-        });
-      
-        if (res.data && res.data.user) {
+          authorization: token
+        }
+      })
+    
+      if (res.data && res.data.user) {
         dispatch({
           type: 'GET_USER_INFO',
           payload: res.data.user,
-        });
-        } else {
-        console.error('User info not found in the response');
-        }
-      } catch (error) {
-        console.error('Error fetching user info:', error);
+        })
+      } else {
+      console.error('User info not found in the response')
       }
-      }
+    } catch (error) {
+      console.error('Error fetching user info:', error)
+    }
+  }
 
-      const logout = async () => {
-        const token = JSON.parse(localStorage.getItem("token"));
-        const res = await axios.delete(API_URL + "/users/logout", {
-          headers: {
-            authorization: token,
-          },
-        });
-        dispatch({
-          type: "LOGOUT",
-          payload: res.data,
-        });
-        if (res.data) {
-          localStorage.removeItem("token");
-        }
-      };
-      
-     
-   
+  const logout = async () => {
+    const token = JSON.parse(localStorage.getItem("token"))
+    const res = await axios.delete(API_URL + "/users/logout", {
+      headers: {
+        authorization: token
+      }
+    })
+    dispatch({
+      type: "LOGOUT",
+      payload: res.data
+    })
+    if (res.data) {
+      localStorage.removeItem("token")
+    }
+  }
 
   return (
     <UserContext.Provider
